8ff1149d79eeca047c87171336d6fac84913a359
lynxariss <lunein863@gmail.com>
2026-09-07 07:49:42 -0600

    Just the basic

---
+++ Funciones.h
@@ -0,0 +1,8 @@
+#ifndef Funciones_H
+
+#define Funciones_H 
+
+//funcion de ingresar una calificacion
+void ();
+
+#endif 
---
+++ main.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "Funciones.h"
+using namespace std;
+
