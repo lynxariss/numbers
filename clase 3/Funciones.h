2c1487cb19917beabd8702db6a9c132c6025bfed
lynxariss <lunein863@gmail.com>
2026-09-10 22:08:45 -0600

    adrian changes

--- clase 3/Funciones.h
+++ clase 3/Funciones.h
@@ -1,6 +1,32 @@
 #ifndef Funciones_H
 #define Funciones_H 
 
+void fillMatrixManually(int matrix[4][4]);
+void fillMatrixRandomly(int matrix[4][4]);
+void displayMatrix(int matrix[4][4], bool marked[4][4]);
 
+bool repeatedNumber(int matrix[4][4], int number);
+
+int checkNumber(
+    int matrix[4][4],
+    bool marked[4][4],
+    int number
+);
+
+int generateNumber(bool drawnNumbers[101]);
+
+void chooseBoard(
+    int matrix[4][4],
+    int player
+);
+
+void startGame(
+    int board1[4][4],
+    int board2[4][4],
+    bool againstComputer
+);
+
+void playAgainstComputer();
+void playAgainstUser();
 
 #endif 
--- clase 3/main.cpp
+++ clase 3/main.cpp
@@ -1,3 +1,44 @@
+
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "Funciones.h"
+
 using namespace std;
+
+int main(int argc, const char* argv[]) {
+    int option = 0;
+
+    srand(time(NULL));
+
+    do {
+        cout << "Options Menu" << endl;
+        cout << "1. Play against the computer." << endl;
+        cout << "2. Play against another player." << endl;
+        cout << "0. Exit the system." << endl;
+        cout << "Select an option: ";
+
+        cin >> option;
+
+        switch (option) {
+        case 1:
+            playAgainstComputer();
+            break;
+
+        case 2:
+            playAgainstUser();
+            break;
+
+        case 0:
+            cout << "Closing system..." << endl;
+            break;
+
+        default:
+            cout << "Invalid option. Please select another option from the menu." << endl;
+            break;
+        }
+
+    } while (option != 0);
+
+    return 0;
+}
